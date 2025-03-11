72df6ac3fe14796bbefe870829419f6ee500e3ef
hadi <121514937+dazbash1@users.noreply.github.com>
2025-03-11 13:11:45 +0330

    78

---
+++ app/78/page.tsx
@@ -0,0 +1,7 @@
+import BottomBar from "@/components/BottomBar";
+
+export default function page() {
+    return (
+        <BottomBar />
+    )
+}
---
+++ components/BottomBar.tsx
@@ -0,0 +1,16 @@
+export default function BottomBar() {
+  return (
+    <div dir="rtl" className="flex flex-row h-screen justify-end px-38 py-26 bg-gray-100 gap-60">
+            <div className="h-24 w-24 bg-gray-200 ">
+            </div>
+            <div className="h-24 w-24 bg-gray-200 ">
+            </div>
+            <div className="h-24 w-24 bg-gray-200 ">
+            </div>
+            <div className="h-24 w-24 bg-gray-200 ">
+            </div>
+            <div className="h-24 w-24 bg-gray-200 ">
+            </div>
+        </div>
+  )
+}
