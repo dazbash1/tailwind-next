045480de178b094f8b267bb38da53ba26072bd50
hadi <121514937+dazbash1@users.noreply.github.com>
2025-03-13 13:53:35 +0330

    Update Modal.tsx

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -19,29 +19,24 @@ export default function Modal() {
                 </div>
             </div>
 
-            <div className="flex flex-row justify-between gap-14">
-                <div className="flex flex-row items-center w-full bg-gray-500 gap-8">
-                    <div className="h-12 w-10 bg-gray-300">
+            <div className="flex flex-row w-full gap-1">
+                <div className="flex flex-row items-center w-full">
+                    <div className="h-5 w-15 bg-gray-300">
                     </div>
-                    <div className="flex flex-row items-center justify-end h-full w-full">
-                        <div className="h-8 w-20 bg-gray-300">
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center">
-                        <div className="h-10 w-10 bg-gray-300">
-
+                    <div className="flex flex-row items-center pr-2">
+                        <div className="h-8 w-6 bg-gray-300">
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-row items-center w-full bg-gray-500 gap-8">
-                    <div className="h-8 w-24 bg-gray-300">
+                <div className="flex flex-row items-center pr-14 gap-1">
+                    <div className=" h-8 w-16 bg-gray-300">
                     </div>
-                    <div className="flex flex-row items-center justify-end h-full w-full">
-                        <div className="h-8 w-20 bg-gray-300">
+                    <div className="flex flex-row items-center justify-end">
+                        <div className="h-8 w-8 bg-gray-300">
                         </div>
                     </div>
-                    <div className="flex flex-row items-center">
-                        <div className="h-10 w-10 bg-red-300">
+                    <div className="flex flex-row items-center pr-7">
+                        <div className="h-5 w-15 bg-gray-300">
 
                         </div>
                     </div>
