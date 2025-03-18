e312155bc127d1192f4d28f21c7f2e38d3864c3c
dazbash1 <121514937+dazbash1@users.noreply.github.com>
2025-03-18 11:55:59 +0330

    Update BottomBar.tsx

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -1,15 +1,25 @@
 export default function BottomBar() {
   return (
-    <div dir="rtl" className="flex flex-row justify-between h-20 px-6 py-6 bg-gray-100 gap-5">
-            <div className="flex-1 bg-gray-400 ">
+    <div dir="rtl" className="flex flex-row justify-between h-20 px-6 py-6 bg-gray-300 gap-5">
+            <div className="flex flex-row flex-1 justify-center items-center">
+              <div className="h-6 w-6 bg-gray-200">
+              </div>
             </div>
-            <div className="flex-1 bg-gray-400 ">
+            <div className="flex flex-row flex-1 justify-center items-center">
+              <div className="h-6 w-6 bg-gray-200">
+              </div>
             </div>
-            <div className="flex-1 bg-gray-400 ">
+            <div className="flex flex-row flex-1 justify-center items-center">
+              <div className="h-6 w-6 bg-gray-200">
+              </div>
             </div>
-            <div className="flex-1 bg-gray-400 ">
+            <div className="flex flex-row flex-1 justify-center items-center">
+              <div className="h-6 w-6 bg-gray-200">
+              </div>
             </div>
-            <div className="flex-1 bg-gray-400 ">
+            <div className="flex flex-row flex-1 justify-center items-center">
+              <div className="h-6 w-6 bg-gray-200">
+              </div>
             </div>
         </div>
   )
