44ae04a6bc53683fe27b07f09dbf75f65f5a2b66
dazbash1 <121514937+dazbash1@users.noreply.github.com>
2025-03-18 11:29:29 +0330

    78

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,16 +1,16 @@
 export default function NavBar() {
     return (
-        <div className="flex flex-row items-end justify-center h-16 bg-gray-500 gap-1">
+        <div className="flex flex-row items-end justify-center h-16 bg-gray-200 gap-1">
             <div className="flex flex-row w-full pr-4 pb-4">
-                <div className="h-4 w-4 bg-gray-300">
+                <div className="h-4 w-4 bg-gray-500">
                 </div>
             </div>
-            <div className="flex flex-row w-full py-4">
-                <div className="h-8 w-20 bg-gray-300">
+            <div className="flex flex-row justify-center w-full py-2">
+                <div className="h-8 w-20 bg-gray-500">
                 </div>
             </div>
-            <div className="flex flex-row justify-end w-full pl-4 pb-4">
-                <div className="h-10 w-20 bg-gray-300">
+            <div className="flex flex-row justify-end w-full pl-4 pb-1">
+                <div className="h-10 w-20 bg-gray-500">
                 </div>
             </div>
         </div>
