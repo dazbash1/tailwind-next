8e72764a211ca955e0cacf5f8e75301512187905
hadi <121514937+dazbash1@users.noreply.github.com>
2025-04-10 11:42:13 +0330

    lkj

--- components/OrderItem2.tsx
+++ components/OrderItem2.tsx
@@ -1,17 +1,17 @@
 export default function OrderItem() {
     return (
-        <div className="flex flex-col justify-between bg-gray-100 h-28 gap-1 p-4 ">
+        <div className="flex flex-col justify-between bg-gray-100 h-28 gap-1 p-4">
             <div className="flex flex-row justify-between">
                 <div className="flex flex-row items-center gap-2 pr-4">
-                    <div className="h-10 w-10 bg-gray-300 ">
+                    <div className="h-10 w-10 bg-gray-300">
                     </div>
-                    <div className="h-8 w-16 bg-gray-300 ">
-                    </div> 
+                    <div className="h-8 w-16 bg-gray-300">
+                    </div>
                 </div>
                 <div className="flex flex-col items-center gap-2 pt-1">
-                    <div className="h-6 w-26 bg-gray-300 ">
+                    <div className="h-6 w-26 bg-gray-300">
                     </div>
-                    <div className="h-6 w-26 bg-gray-300 ">
+                    <div className="h-6 w-26 bg-gray-300">
                     </div>
                 </div>
             </div>
