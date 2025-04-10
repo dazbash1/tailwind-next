7e8ff652f534d99b3d7b3754d4dda7d79677d5bd
hadi <121514937+dazbash1@users.noreply.github.com>
2025-04-10 10:33:08 +0330

    q

---
+++ app/87/page.tsx
@@ -0,0 +1,8 @@
+import PriceItdm2 from "@/components/PriceItem2";
+export default function page() {
+    return (
+        <div>
+             <PriceItdm2/>
+        </div>
+    )
+}
---
+++ components/PriceItem2.tsx
@@ -0,0 +1,18 @@
+export default function PriceItem2() {
+    return (
+        <div className="flex flex-row justify-between gap-2 bg-gray-500 h-20">
+            <div className="flex flex-row items-center gap-2 pr-4">
+                <div className="h-10 w-10 bg-gray-300 ">
+                </div>
+                <div className="h-6 w-16 bg-gray-300 ">
+                </div>
+            </div>
+            <div className="flex flex-row items-center justify-end pl-4">
+                <div className="flex flex-row items-center justify-center ">
+                    <div className="h-6 w-24 bg-gray-300 ">
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
